43799088e545cb23017d7fc21510a0374e61c394
Alex <aspnet123@gmail.com>
2016-10-23 02:19:56 +0300

    third commit

--- scraper/scraper.go
+++ scraper/scraper.go
@@ -3,30 +3,30 @@ package scraper
 import (
 	"fmt"
 	"net/http"
+	"golang.org/x/net/html"
 	"github.com/PuerkitoBio/goquery"
+	"io"
+	"log"
+	"os"
 )
-
-type articleDescription struct {
-	Url             string
-	TitleHtml       string
-	TitleText       string
-	PublicationDate string
-	Source          string
-	SnippetHtml     string
-	ArticleSnippet  string
-}
 
 func GetNews(url string) []articleDescription {
 	fmt.Println("scraper started")
 	var articles []articleDescription
 	resp := GetResponse(url)
 	defer resp.Body.Close()
+	//printBody(resp)
 	fmt.Println(resp.Header)
 	doc, err := goquery.NewDocumentFromResponse(resp)
-	if(err != nil) {
+	if (err != nil) {
 		panic(err)
 	}
-	fmt.Println(doc.Nodes)
+	//var articleNodes = HtmlDoc.DocumentNode.SelectNodes("//td[@class='esc-layout-article-cell']");
+	articleNodes :=doc.Find("td.esc-layout-article-cell")
+	var firstNode html.Node = articleNodes.Nodes[0]
+	fmt.Println(firstNode)
+	fmt.Println(articleNodes)
+
 
 	return articles
 }
@@ -38,3 +38,12 @@ func GetResponse(url string) (resp *http.Response) {
 	}
 	return
 }
+
+func printBody(resp *http.Response) {
+	_, err := io.Copy(os.Stdout, resp.Body)
+	if err != nil {
+		log.Fatal(err)
+	}
+}
+
+
---
+++ scraper/type.go
@@ -0,0 +1,11 @@
+package scraper
+
+type articleDescription struct {
+	Url             string
+	TitleHtml       string
+	TitleText       string
+	PublicationDate string
+	Source          string
+	SnippetHtml     string
+	ArticleSnippet  string
+}
