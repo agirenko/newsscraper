7012e9392ced949ddb858c75381d0a4b07215627
Alex <aspnet123@gmail.com>
2016-10-23 01:35:31 +0300

    second commit

--- main/main.go
+++ main/main.go
@@ -3,7 +3,7 @@ package main
 import (
 	"github.com/agirenko/newsscraper/scraper"
 )
-
+const url = "http://news.google.com/news?hl=de&pz=1&ned=us&q=lottery+news"
 func main() {
-	scraper.GetNews();
+	scraper.GetNews(url);
 }
--- scraper/scraper.go
+++ scraper/scraper.go
@@ -2,8 +2,39 @@ package scraper
 
 import (
 	"fmt"
+	"net/http"
+	"github.com/PuerkitoBio/goquery"
 )
 
-func GetNews() {
-	fmt.Println("abc")
+type articleDescription struct {
+	Url             string
+	TitleHtml       string
+	TitleText       string
+	PublicationDate string
+	Source          string
+	SnippetHtml     string
+	ArticleSnippet  string
+}
+
+func GetNews(url string) []articleDescription {
+	fmt.Println("scraper started")
+	var articles []articleDescription
+	resp := GetResponse(url)
+	defer resp.Body.Close()
+	fmt.Println(resp.Header)
+	doc, err := goquery.NewDocumentFromResponse(resp)
+	if(err != nil) {
+		panic(err)
+	}
+	fmt.Println(doc.Nodes)
+
+	return articles
+}
+
+func GetResponse(url string) (resp *http.Response) {
+	resp, err := http.Get(url)
+	if err != nil {
+		panic(err)
+	}
+	return
 }
